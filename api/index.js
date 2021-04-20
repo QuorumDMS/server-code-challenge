ebf5035bfe1044f6e603406dc98a767798d72ea3
Matt Forster <hey@mattforster.ca>
2021-04-20 16:24:54 -0600

    chore: clarify docker listen requirements

--- api/index.js
+++ api/index.js
@@ -1,1 +1,5 @@
-// Port to listen to: 8888, (set in ../docker-compose.yaml) available as `process.env.PORT` here
+// Listen to: 0.0.0.0:8888, (set in ../docker-compose.yaml) available as `process.env.PORT` here
+
+// restify: await server.listen(process.env.PORT, '0.0.0.0');
+// express: await app.listen(process.env.PORT, '0.0.0.0');
+// fastify: await fastify.listen(process.env.PORT, '0.0.0.0');
